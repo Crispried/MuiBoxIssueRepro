16219aa75468dcf4530693f84e0ae9ce59005107
ruslan.avsenin <ruslan.avsenin@orckestra.com>
2020-05-27 23:08:07 +0300

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const unexpected = require("unexpected");
+const unexpectedDom = require("unexpected-dom");
+const unexpectedReaction = require("unexpected-reaction");
+
+global.expect = unexpected.clone().use(unexpectedDom).use(unexpectedReaction);
+
+describe("App", () => {
+  it("renders App", () => {
+    return expect(
+      <App />,
+      "when mounted",
+      "to satisfy",
+      <Grid container>
+        <Grid item xs>
+          <Box>
+            <p>Hello</p>
+          </Box>
+        </Grid>
+        <Grid item xs>
+          <Container>
+            <p>World</p>
+          </Container>
+        </Grid>
+      </Grid>
+    );
+  });
 });
